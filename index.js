b196179903e7240cbda81710a2cc9e0b322cc50b
beautiful-collection-0798 <opoon01@gmail.com>
2019-01-14 14:11:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   let returnArray = [];
-  
+  for(let counter = 0; counter < musicians.length; counter++){
+    let plays = musicians[counter] + ' plays ' + instruments[counter];
+    returnArray.push(plays);
+  }
+  return returnArray;
 }
